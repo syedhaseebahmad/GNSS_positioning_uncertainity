2e3862452e999d977cd09a6096ee7aca85ebc04f
Syed Haseeb Ahmad <syedhaseebahmad97@gmail.com>
2023-04-27 14:45:23 +0200

    procfile added

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import pickle
 import os
 import pandas as pd
-from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from flask import Flask,request,app,jsonify,url_for,render_template
+
 
 
 app = Flask(__name__)
 ## Load the model
-randomforestmodel = pickle.load(open('GNSS_positioning_uncertainity/randomforestmodel.pkl','rb'))
+randomforestmodel = pickle.load(open('randomforestmodel.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -22,6 +24,16 @@ def predict_api():
     output = randomforestmodel.predict(data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+   # final_input = StandardScaler.transform(np.array(data).reshape(1,18))
+    final_input = np.array(data).reshape(1,18)
+    print(final_input)
+    output = randomforestmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The predicted uncertainity is {}".format(output) )
+
 
 if __name__ == "__main__":
     app.run(debug= True)
